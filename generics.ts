1467938dd714e2ee174419357ccd32c24142acef
Sandhya Srinivasan <sandy150225@gmail.com>
2023-04-18 18:51:09 +0200

    generics flexibility

--- generics.ts
+++ generics.ts
@@ -18,16 +18,34 @@ promise.then((data) => {
 });
 
 
-//------------------------------------------CREATING A GENERIC FUNCTION-------------------------------------
+//------------------------------------CREATING A GENERIC FUNCTION-------------------------------------
 
 // Here we have created a generic Type which takes 2 values, both of type Object
 // The type of the subtype can be assigned by extending the type that you want
 // Like below: T extends object (or) T extends string
+// By extending the type we are creating a CONSTRAINT on the type
 function combineObjects<T extends object, U extends object> (obj1: T, obj2: U) { 
     return Object.assign(obj1, obj2)
 }
 
 const result = combineObjects({name: 'Sandhya'}, {profession: 'Developer'})
 console.log(result)
+
+
+//----------------------------------Another cool example!--------------------------------------------
+interface propertyLength {
+    length: number
+}
+
+function returnSize<T extends propertyLength>(data: T) {
+    return data.length //Here the function would not know the type of data and would complain when trying to access length property
+    // To avoid the error we have extended the type with an interface which has a length property
+    // TS would now know the input must have a length property
+}
+
+console.log(returnSize('sandhya'))
+console.log(returnSize([1,2,4]))
+// returnSize(2) // This would throw an error
+
 
 
